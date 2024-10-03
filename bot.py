f53ae942128edfbbe0b188ca864615ef3a7bd9d2
Lirit Dampier <2560877@dundee.ac.uk>
2024-10-04 00:35:49 +0100

    class changes + user input

--- bot.py
+++ bot.py
@@ -9,10 +9,81 @@ botSock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 
 serverName = 'localHost IPv6'
 
+def checkNick(nickname):
+    # 1 < nickname <= 15
+    flag = False
+    
+    if len(nickname) == 0:
+        # name is too short
+        flag = False
+        print("Le pseudo saisi est trop court")
+    elif len(nickname) <= 15:
+        # name is good length
+        flag = True
+    else:
+        # name is too long
+        flag = False
+        print("Le pseudo saisi est trop long")
+        
+    # while the flag is false, user must enter a new nickname of correct length
+    while flag==False:
+        nickname = input("entrez un surnom pour vous-même: ")
+        if len(nickname) == 0:
+            # name is too short
+            flag = False
+            print("Le pseudo saisi est trop court")
+        elif len(nickname) <= 15:
+            # name is good length
+            flag = True
+        else:
+            # name is too long
+            flag = False
+            print("Le pseudo saisi est trop long")
+            
+    return nickname
+
+def checkChannel(channelName):
+    # 1 < channelName <= 15
+    flag = False
+    
+    if len(channelName) == 0:
+        # name is too short
+        flag = False
+        print("le nom de la chaîne est trop court")
+    elif len(channelName) <= 15:
+        # name is good length
+        flag = True
+    else:
+        # name is too long
+        flag = False
+        print("le nom de la chaîne est trop court")
+    
+    # if the flag is false (aka the entered name is invalid), user must enter a new channelName of correct length
+    while flag==False:
+        channelName = input("Saisissez le nom de la chaîne que vous souhaitez rejoindre: ")
+        if len(channelName) == 0:
+            # name is too short
+            flag = False
+            print("le nom de la chaîne est trop court")
+        elif len(channelName) <= 15:
+            # name is good length
+            flag = True
+        else:
+            # name is too long
+            flag = False
+            print("le nom de la chaîne est trop long")
+    
+    channelName = "#"+channelName
+    return channelName
+
 HOST = '::1' #host name
 PORT = 6667 #port number
-NICK = 'Ludovic' #sets default nickname for bot
-CHANNEL = '#hello'
+#NICK = 'Ludovic' #sets default nickname for bot
+#CHANNEL = '#hello'
+nickname = input("entrez un surnom pour vous-même: ")
+NICK = checkNick(nickname)
+channelName = input("Saisissez le nom de la chaîne que vous souhaitez rejoindre: ")
+CHANNEL = checkChannel(channelName)
 
 if len(sys.argv) > 0:
     for i in range(1, len(sys.argv)):
@@ -40,25 +111,29 @@ if len(sys.argv) > 0:
 class Bot:
     
     botSock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-    global NICK
-    NICK = 'Ludovic' #sets default nickname for bot
+    #global NICK
+    #NICK = 'Ludovic' #sets default nickname for bot
     
-    def __init__(self):
-        pass
+    def __init__(self, nickname, channel):
+        self.nickname = nickname
+        self.channel = channel
     
-    def getNick():
-        global NICK
-        return NICK
+    def returnNick(self):
+        return str(self.nickname)
+    
+    def returnChannel(self):
+        return str(self.channel)
 
-    def log_in():
+    def log_in(self):
+        nick = self.returnNick()
         botSock.send(bytes("CAP LS 302\r\n", "UTF-8")) #CAP command used for sign in, idk what this does but its the lynch pin holding the sign in protocol together apparently
         time.sleep(0.01)
-        botSock.send(bytes("NICK " + NICK + "\r\n", "UTF-8")) #nickname is requested
-        botSock.send(bytes("USER " + NICK + " 0 * :realname\r\n", "UTF-8")) #nickname is given to the bot
+        botSock.send(bytes("NICK " + nick + "\r\n", "UTF-8")) #nickname is requested
+        botSock.send(bytes("USER " + nick + " 0 * :realname\r\n", "UTF-8")) #nickname is given to the bot
         time.sleep(0.1)
         botSock.send(bytes("CAP END\r\n", 'UTF-8')) #close CAP
         time.sleep(0.1)
-        botSock.send(bytes("JOIN #test\r\n", "UTF-8")) #test channel is joined
+        botSock.send(bytes("JOIN " + self.returnChannel() + "\r\n", "UTF-8")) #test channel is joined
         time.sleep(0.1)
 
     # ^^^ sleeps used to break commands into seperate lines and wait for a response if neccesary
@@ -70,29 +145,31 @@ class Bot:
         botSock.send(bytes(message + '\r\n', 'UTF-8'))
 
     def getText(self):
+        channel = self.returnChannel()
+        nick = self.returnNick()
         text = botSock.recv(2040) #reads text sent by server to the bot. This will be expanded to do the pre generated responses to user messages etc. 
         text = text.decode() #converts the bytes to string
         if text.find('PING') != -1: #if the text is a ping
             self.sendIRC('PONG ' + socket.gethostname()) #replies with a pong
             print("PONG sent to server") #check if PONG is sent
-        elif text.find('PRIVMSG ' + CHANNEL + ' :!hello') != -1:
+        elif text.find('PRIVMSG ' + channel + ' :!hello') != -1:
             splitText = text.split(':')
             splitText = splitText[1].split('!') # splits the string to find the user name of the sender
             name = splitText[0]
             if random.choice([0,1]) == 0: # 50/50 chance to respond with one of two greetings
-                self.sendMsg('Salut, ' + name + '!', CHANNEL)
+                self.sendMsg('Salut, ' + name + '!', channel)
                 print('Salut, ' + name + '!')
             else:
-                self.sendMsg('Bonjour, ' + name + '!', CHANNEL)
+                self.sendMsg('Bonjour, ' + name + '!', channel)
                 print('Bonjour, ' + name + '!')
-        elif text.find('PRIVMSG ' + CHANNEL + ' :!slap') != -1:
+        elif text.find('PRIVMSG ' + channel + ' :!slap') != -1:
             #here we will randomly choose a user
-            self.sendMsg("TEMPUSER, tu as été giflé avec une truite !", CHANNEL)
-        elif text.find('PRIVMSG ' + NICK) != -1:
+            self.sendMsg("TEMPUSER, tu as été giflé avec une truite !", channel)
+        elif text.find('PRIVMSG ' + nick) != -1:
             splitText = text.split(':')
             splitText = splitText[1].split('!') # splits the string to find the user name of the sender
             name = splitText[0]
-            self.sendMsg(Bot.getFact(), name)
+            self.sendMsg(self.getFact(), name)
         return text
 
     def sendMsg(message, target):
@@ -101,7 +178,8 @@ class Bot:
     def sendIRC(message):
         botSock.send(bytes(message + '\r\n', 'UTF-8'))
 
-    def joinChannel(self, channel):
+    def joinChannel(self):
+        channel = self.returnChannel()
         self.sendIRC('JOIN ' + channel) #functions that either don't work or currently aren't in use
         self.setChannel(channel)
 
@@ -113,7 +191,6 @@ class Bot:
         fact = random.choice(lines)
         print(fact)
         return fact
-
 
     # Retaining the initial information sent by miniircd about the channel and its users
     def storeInitialInfo(self):
@@ -129,11 +206,17 @@ class Bot:
     def returnUsers(self):
         #channelUsers = [] # list of users
         # use NAMES command to get all nicknames that are visible on the channel 'test'
-        botSock.send(bytes("WHO #test\r\n", "UTF-8"))
-        names =self.getText(self)
-        #print(names)
+        botSock.send(bytes("WHO \r\n", "UTF-8"))
+        names =self.getText()
+        print(names)
         str(names)
-        users = names[3:]
+        users = names[1:]
+        index = str(users).find("#")
+        users = users[:index-1]
+        channelUsers = list(users.split(" "))
+        return channelUsers
+        
+        '''users = names[5:]
         #print(users)
         index = str(users).find(":")
         #print(index)
@@ -145,42 +228,41 @@ class Bot:
         users = users[2:-1]
         #print(users)
         channelUsers = list(users.split(" "))
-        return channelUsers
+        return channelUsers'''
 
-    # function to get the channel name
-    def getChannel(self):
+    # function to get the current channel name
+    def getCurrentChannel(self):
         # use NAMES command to get all nicknames that are visible on the channel 'test' and the channel name
-        botSock.send(bytes("WHO #test\r\n", "UTF-8"))
-        names =self.getText(self)
+        botSock.send(bytes("WHO" + self.returnChannel() + "\r\n", "UTF-8"))
+        names = self.getText()
         index = str(names).find("#")
         channel = str(names)
         index1 = channel[index:]
         index2 = str(index1).find(":")
         channelName = channel[index:(index+index2)]
         return channelName
-    
-    # function to return the channel name
-    def returnChannelName(self):
-        currentChannel = self.getChannel()
-        return currentChannel
+
 
 try:
     botSock.connect((HOST, PORT))
-    Bot.log_in()
-    print(Bot.storeInitialInfo(Bot))
+    ludovic = Bot(NICK, CHANNEL)
+    print(NICK)
+    ludovic.log_in()
+    #print(ludovic.storeInitialInfo())
     #print(Bot.returnUsers(Bot))
 
-    Bot.sendMsg(f"Bonjour, je m'appelle {Bot.getNick()} et je suis chatbot sur ce serveur", CHANNEL) # sends a message to the test channel
+    Bot.sendMsg(f"Bonjour, je m'appelle {ludovic.returnNick()} et je suis chatbot sur ce serveur", CHANNEL) # sends a message to the test channel
     
     # testing for returning channel + users:
-    print(f'Channel name: {Bot.getChannel(Bot)}')
-    print(f'Users : {Bot.returnUsers(Bot)}')
+    print(f'Channel name: {ludovic.getCurrentChannel()}')
+    print(f'Users : {ludovic.returnUsers()}')
     
     while 1: #while loop prevents bot from disconnecting once it runs out of preset commands
-        text = Bot.getText(Bot)
+        text = ludovic.getText()
         print(text) #any recieved text is printed for debugging purposes
         
 except Exception as e:
+    raise
     print(f"port indisponible ou n'existe pas: {e}")
 finally:
     botSock.close()
